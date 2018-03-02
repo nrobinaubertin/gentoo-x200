da4a606b839b4419bc8e639914dc7d244da87e15
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-03-02 08:47:56 +0100

    Add hosts file

--- save_config.sh
+++ save_config.sh
@@ -2,13 +2,25 @@
 
 mkdir -p HOME portage cargo kernel
 
+# kernel
 cp /usr/src/linux/.config kernel/
+
+# world
 cp /var/lib/portage/world world
+
+# HOME
 cp ${HOME}/.xinitrc HOME/
 cp ${HOME}/.bash_profile HOME/
+
+# cargo
 cargo install --list > cargo/list.txt
+
+# portage
 cp -r /etc/portage/savedconfig portage
 cp -r /etc/portage/repos.conf portage
 cp /etc/portage/make.conf portage
 cp /etc/portage/package.use portage
 cp /etc/portage/package.accept_keywords portage
+
+# etc
+cp /etc/hosts etc
