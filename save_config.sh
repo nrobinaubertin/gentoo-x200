eaac6d8610a644ee4dd4a6850093cbd8979351ae
Niels Robin-Aubertin <git@niels.fr>
2018-11-25 10:10:52 +0100

    Don't save saveconfig directory anymore

--- save_config.sh
+++ save_config.sh
@@ -19,7 +19,6 @@ cp ${HOME}/.bash_profile HOME/
 cargo install --list > cargo/list.txt
 
 # portage
-cp -r /etc/portage/savedconfig portage
 cp -r /etc/portage/repos.conf portage
 cp /etc/portage/make.conf portage
 cp /etc/portage/package.* portage
