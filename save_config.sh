294db68e62563e0eda49b92a5f162e5866913386
Niels Robin-Aubertin <git@niels.fr>
2019-09-18 09:34:49 +0200

    Update config, remove firefox config (now in my dotfiles)

--- save_config.sh
+++ save_config.sh
@@ -31,11 +31,3 @@ cp -r /usr/local/portage/ ebuilds/
 
 # save pip installs
 pip freeze --user > pip/requirements.txt
-
-# firefox
-for profile in $(find $HOME/.mozilla/firefox -maxdepth 1 -mindepth 1 -type d); do
-    profile_name="$(basename $profile)"
-    mkdir -p "firefox/$profile_name"
-    [ -d "$profile/chrome" ] && cp -r "$profile/chrome" "firefox/$profile_name/chrome"
-    [ -f "$profile/user.js" ] && cp "$profile/user.js" "firefox/$profile_name/user.js"
-done
